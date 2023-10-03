17bcba62b399a036e85bb00ffd0a14d770157c80
diogoscastro <diogo.castro@fatec.gov.br>
2023-10-03 12:31:22 -0300

    Aula0310 componentes e funções

--- react_basico/basic-app/src/components/AppViewer.jsx
+++ react_basico/basic-app/src/components/AppViewer.jsx
@@ -1,6 +1,15 @@
 import styles from "./AppViewer_module.css";
+//camada de compilação de serviços
+function somaNumeros(n1, n2) {
+  return n1 + n2;
+}
 const AppViewer = () => {
-  return <div className={styles.avContainer}>AppViewer</div>;
+  return (
+    <>
+      <div className={styles.avContainer}>AppViewer</div>;
+      <p>{somaNumeros(5, 17)}</p>
+    </>
+  );
 };
 
 export default AppViewer;
