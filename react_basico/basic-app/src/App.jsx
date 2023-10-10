728f71c1bbea50b1a55df223f4e03018320bedc6
diogoscastro <diogo.castro@fatec.gov.br>
2023-10-10 12:30:26 -0300

    aula 1010

--- react_basico/basic-app/src/App.jsx
+++ react_basico/basic-app/src/App.jsx
@@ -4,13 +4,19 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import AppViewer from "./components/AppViewer";
 
-function App() {
-  const [count, setCount] = useState(0);
+const mudarNumero = (n) => {
+  console.log(n);
+  return n + 1;
+};
+let numero = 15;
 
+function App() {
   return (
     <>
       <h1>Meu Primeiro webapp</h1>
       <AppViewer />
+      <p>Número: {numero}</p>
+      <button onClick={mudarNumero(numero)}>Apertar e Mudar</button>
     </>
   );
 }
--- react_basico/basic-app/src/components/AppViewer.jsx
+++ react_basico/basic-app/src/components/AppViewer.jsx
@@ -1,14 +1,132 @@
 import styles from "./AppViewer_module.css";
 //camada de compilação de serviços
-function somaNumeros(n1, n2) {
-  return n1 + n2;
-}
+
 const AppViewer = () => {
+  const professores = [
+    // Primeiro objeto
+    {
+      id: 1,
+      nome: "José Armando Vargas",
+      idade: 42,
+      formacao: "Engenharia da Computação",
+      titulacao: "Latu-Senso",
+    },
+
+    // Segundo objeto com valores diferentes
+    {
+      id: 2,
+      nome: "Maria Silva",
+      idade: 35,
+      formacao: "Ciência da Informação",
+      titulacao: "Mestrado",
+    },
+
+    // Terceiro objeto com valores diferentes
+    {
+      id: 3,
+      nome: "João Pereira",
+      idade: 28,
+      formacao: "Engenharia Elétrica",
+      titulacao: "Doutorado",
+    },
+
+    // Repita o processo para criar outros objetos com valores diferentes
+
+    // Quarto objeto
+    {
+      id: 4,
+      nome: "Ana Souza",
+      idade: 38,
+      formacao: "Administração de Empresas",
+      titulacao: "Bacharelado",
+    },
+
+    // Quinto objeto
+    {
+      id: 5,
+      nome: "Pedro Santos",
+      idade: 45,
+      formacao: "Medicina",
+      titulacao: "Doutorado",
+    },
+
+    // Sexto objeto
+    {
+      id: 6,
+      nome: "Carla Lima",
+      idade: 32,
+      formacao: "Psicologia",
+      titulacao: "Mestrado",
+    },
+
+    // Sétimo objeto
+    {
+      id: 7,
+      nome: "Rafaela Ferreira",
+      idade: 27,
+      formacao: "Arquitetura",
+      titulacao: "Bacharelado",
+    },
+
+    // Oitavo objeto
+    {
+      id: 8,
+      nome: "Fernando Oliveira",
+      idade: 50,
+      formacao: "Economia",
+      titulacao: "Doutorado",
+    },
+
+    // Nono objeto
+    {
+      id: 9,
+      nome: "Mariana Costa",
+      idade: 29,
+      formacao: "Direito",
+      titulacao: "Bacharelado",
+    },
+
+    // Agora você tem nove objetos com valores diferentes
+  ];
+
   return (
-    <>
-      <div className={styles.avContainer}>AppViewer</div>;
-      <p>{somaNumeros(5, 17)}</p>
-    </>
+    <div>
+      <h2>Lista de Professores</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>Idade</th>
+            <th>Formação</th>
+            <th>Titulação</th>
+          </tr>
+        </thead>
+        <tbody>
+          {professores.map((professor) => (
+            <tr
+              className={professor.id % 2 == 1 ? styles.linhacinza : null}
+              key={professor.id}
+            >
+              <td>{professor.id}</td>
+              <td>{professor.nome}</td>
+              <td>{professor.idade}</td>
+              <td>{professor.formacao}</td>
+              <td
+                className={
+                  professor.titulacao == "Mestrado" ||
+                  professor.titulacao == "Doutorado"
+                    ? styles.superTitulo
+                    : styles.normalTitulo
+                }
+              >
+                {professor.titulacao}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
